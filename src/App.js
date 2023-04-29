a496e7d3d56854f1db9c3e443f8abac1b2bf5449
fracabu <100199965+fracabu@users.noreply.github.com>
2023-04-29 11:23:22 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
+
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    getTrendingMovieData("movie");
+  }, []);
+
+
+  async function getTrendingMovieData(type) {
+    try {
+      const apiKey = '06adf808dcf2297eee97f0494253138a';
+      let resp = await axios.get(`https://api.themoviedb.org/3/trending/${type}/day?api_key=${apiKey}&media_type=movie`);
+      console.log(21, resp.data.results);
+
+      setMovieData(resp.data.results);
+
+    } catch (e) {
+
+
+    } finally {
+
+    }
+
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="background_container">
+        <div className="button_container">
+          <button onClick={() => {
+            getTrendingMovieData("movie");
+          }
+          }>
+            Trending Movies
+          </button>
+          <button onClick={() => {
+            getTrendingMovieData("tv");
+          }
+          }>
+            Trending TV
+          </button>
+        </div>
+        <div className='flex-container'>
+          {movieData.map((item) =>
+            <div className="movie_item">
+              <img src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`} />
+              <div className="movie_name">
+                {item.original_title ? item.original_title : item.original_name}
+
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+
   );
 }
 
